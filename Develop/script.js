00133a9a7e7da0d450e7dc9f77747569e9f491ff
Zach LaFleur <zach.lafleur95@gmail.com>
2020-08-20 18:27:17 -0600

    added for each function to change the time in the timeblock. began looking at local storage

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,8 @@ const timeBlockTemp = `
     <div class="col-md-2"><button class="saveBtn"></button> </div>
 </div>`;
 let currentDay = document.querySelector("#currentDay");
+// Creating an array to store the hours of the day
+const hoursOfDay = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
 
 $(document).ready(function () {
     // Establishing the current time and date on the top
@@ -16,16 +18,27 @@ $(document).ready(function () {
         }, 1000);
     }
     date();
-    // For loop creating 24 timeblock containers for the daily planner
-    for (let i = 0; i < 8; i++) {
+    // For loop creating 8 timeblock containers for the daily planner
+    for (let i = 0; i <= 8; i++) {
         const foo = $($.parseHTML(timeBlockTemp));
         $('#timeBlockCont').append(foo);
     };
     // Creating an onclick listener for each of the time blocks
-    $(".time-block").on("click", function() {
-        console.log("My Old Friend")
+    // $(".time-block").on("click", function() {
+    //     console.log("My Old Friend")
+    // });
+    // For each loop for time blocks
+    let index = 0;
+    $(".hour").each(function() {
+        $(this).text(hoursOfDay[index]);
+        index = index + 1
     });
-    // Creating an array to store the hours of the day
-    const hoursOfDay = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
-    
+    // save button
+    $(".saveBtn").on("click", function() {
+        let value = $(this).closest(".time-block").val();
+        let hourBlock = $(this).siblings(".hour").val();
+        localStorage.setItem(hourBlock, value);
+        console.log(value, hourBlock);
+    })
+
 });
